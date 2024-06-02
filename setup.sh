152e2f1667ed03a7f7efc437274c818c3d9475a3
yashkurkure <yash.kurkure1903@gmail.com>
2024-06-01 21:40:39 -0500

    rm ansible_runner

---
+++ setup.sh
@@ -0,0 +1,12 @@
+ansible-playbook \
+    -i /local/cluster_inventory.yml\
+    /local/repository/ansible/nfs.yml
+
+ansible-playbook \
+    -i /local/cluster_inventory.yml\
+    /local/repository/ansible/mpich.yml
+
+ansible-playbook \
+    -i /local/cluster_inventory.yml\
+    --extra-vars "username=ykurkure"\
+    /local/repository/ansible/ssh.yml
