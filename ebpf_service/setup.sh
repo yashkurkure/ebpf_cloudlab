c681f7fb4e1fd65245536c0c241e087063327562
yashkurkure <yash.kurkure1903@gmail.com>
2024-06-17 14:00:43 -0500

    cp error

--- ebpf_service/setup.sh
+++ ebpf_service/setup.sh
@@ -5,4 +5,3 @@
 sudo cp /local/repository/ebpf_service/ebpf.service /etc/systemd/system/ebpf.service
 sudo systemctl enable ebpf.service
 sudo systemctl start ebpf.service
-touch /pbsusers/service.log
